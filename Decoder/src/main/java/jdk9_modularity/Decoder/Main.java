548b1889b8ceb263cb541115c37422f37bc6ed53
Banee Ishaque K <baneeishaque@gmail.com>
2019-07-26 03:29:22 +0530

    Initial - Gradle multi project

---
+++ Decoder/src/main/java/jdk9_modularity/Decoder/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdk9_modularity.Decoder;
+
+/**
+ *
+ * @author srf
+ */
+import jdk9_modularity.Reader.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.format("Reading with a %s!%n", Scanner.name());
+    }
+}
