60a1e7d13a131eac1fea95dad36602f41b05bde0
Banee Ishaque K <baneeishaque@gmail.com>
2019-07-26 04:11:38 +0530

    Jigsaw Fix

---
+++ Reader/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module jdk9_modularity.Reader {
+    exports jdk9_modularity.Reader;
+}
